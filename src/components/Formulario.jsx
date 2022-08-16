8922f10e67ea226fe126f26cd36d37e7a0092434
drocioferrer <drocioferrer@gmail.com>
2022-08-16 16:03:44 -0300

    Se agregan variables de entorno para Desarrollo y Produccion

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -30,7 +30,7 @@ const Formulario = ({ cliente, cargando }) => {
             let respuesta
             if (cliente.id) {
                 // EDITAR REGISTRO
-                const url = `http://localhost:4000/clientes/${cliente.id}`
+                const url = `${import.meta.env.VITE_API_URL}/${cliente.id}`
                 respuesta = await fetch(url, {
                     method: 'PUT',
                     body: JSON.stringify(values),
@@ -41,7 +41,7 @@ const Formulario = ({ cliente, cargando }) => {
 
             } else {
                 // NUEVO REGISTRO
-                const url = 'http://localhost:4000/clientes'
+                const url = import.meta.env.VITE_API_URL
                 respuesta = await fetch(url, {
                     method: 'POST',
                     body: JSON.stringify(values),
